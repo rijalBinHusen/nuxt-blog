c7875bb700389d6f8cd6a1e4398324f5d28d0171
rijal <jeryzal333@gmail.com>
2022-08-27 03:28:49 +0700

    displaying blog

---
+++ pages/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <article>
+    <h1>{{ post.title }}</h1>
+    <nuxt-content :document="post" />
+  </article>
+</template>
+
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      const slug = params.slug;
+      const post = await $content(slug).fetch();
+
+      return { post };
+    },
+  };
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,8 @@
 <script>
 export default {
     async asyncData({ $content, params }) {
-      const posts = await $content().fetch();
+      // const posts = await $content().fetch();
+      const posts = await $content().only(["slug", "title"]).fetch();
       return { posts };
     },
   name: 'IndexPage'
