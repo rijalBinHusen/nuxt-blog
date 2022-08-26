9d0833a71e42cbf3f31c7da26332451fbdadf39e
rijal <jeryzal333@gmail.com>
2022-08-27 03:20:21 +0700

    listing blog without style

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <Tutorial/>
+  <!-- <Tutorial/> -->
+    <ul>
+    <li v-for="post in posts" :key="post.slug">
+      <nuxt-link :to="`/${post.slug}`"> {{ post.title }} </nuxt-link>
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
+    async asyncData({ $content, params }) {
+      const posts = await $content().fetch();
+      return { posts };
+    },
   name: 'IndexPage'
 }
 </script>
