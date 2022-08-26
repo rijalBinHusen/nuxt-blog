c71211b499863ae18c198529fc50f87879069113
rijal <jeryzal333@gmail.com>
2022-08-27 03:30:52 +0700

    table of content

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,6 +1,13 @@
 <template>
   <article>
     <h1>{{ post.title }}</h1>
+    <nav>
+      <ul>
+        <li v-for="link of post.toc" :key="link.id">
+          <nuxt-link :to="`#${link.id}`">{{ link.text }}</nuxt-link>
+        </li>
+      </ul>
+    </nav>
     <nuxt-content :document="post" />
   </article>
 </template>
